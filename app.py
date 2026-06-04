0bcb5c19810a4f60afd4a96cbdf14dac94d4d673
AvynaashD <avypandit@gmail.com>
2026-06-04 13:17:33 +0530

    Fix prompt import paths

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
 # 1. Page Configuration
